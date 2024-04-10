27057b7e14f847d9eba0d162fb01e8d44efbf026
Muluken <mulukeneshetu70@gmail.com>
2024-04-09 22:24:07 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#install pupplet lint
-package { 'puppet-lint':
-  ensure   => '1.1.0',
-  provider => 'gem',
-}
+# how to install a package using puppet
+
+package { 'flask':
+  ensure => "installed"
+  }
