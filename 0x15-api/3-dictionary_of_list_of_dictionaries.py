e61fa2d9d77fa129f5276948eeecca5a7a2ad919
Muluken <mulukeneshetu70@gmail.com>
2024-04-25 05:53:22 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,27 @@
 #!/usr/bin/python3
-# script to gather all todo data from an API and write to JSON file
-import json
-import requests
-import sys
+"""Exports data in the JSON format"""
 
+if __name__ == "__main__":
 
-def get_username(base_url, user_id):
-    """Gets username
-       Args:
-           base_url (str): base url for API
-           user_id (str): user id number
-       Returns: username
-    """
-    response = requests.get(
-        "{}users/{}".format(base_url, user_id), verify=False)
-    usr_dict = response.json()
-    return usr_dict['username']
+    import json
+    import requests
+    import sys
 
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
-def get_complete_todo_list(base_url):
-    """Gets complete todo list
-       Args:
-           base_url (str): base url for API
-       Returns: list of todo items (dicts)
-    """
-    response = requests.get(
-        "{}todos".format(base_url), verify=False)
-    return response.json()
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
 
-
-if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    users_dict = {}
-    todo_dict = {}
-    todo_list = get_complete_todo_list(base_url)
-
-    with open('todo_all_employees.json', 'w') as f:
-        uname = None
-        for task in todo_list:
-            user_id = str(task['userId'])
-            if user_id in users_dict.keys():
-                uname = users_dict[user_id]
-            else:
-                uname = get_username(base_url, user_id)
-                users_dict[user_id] = uname
-                todo_dict[user_id] = []
-
-            todo = {}
-            todo['task'] = task['title']
-            todo['completed'] = task['completed']
-            todo['username'] = uname
-            todo_dict[user_id].append(todo)
-
-        f.write(json.dumps(todo_dict))
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
