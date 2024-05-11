375df4441603ccd3aee0f0526e2bc3c3e5d78caf
Muluken <mulukeneshetu70@gmail.com>
2024-05-11 11:19:38 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,14 +3,15 @@
 import requests
 
 
+import requests
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom user agent'}
+    headers = {'User-Agent': 'Custom User Agent'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data['data']['subscribers']
     else:
         return 0
