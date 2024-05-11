a0ddc17b7c131d59819805a7f0359137094151ee
Muluken <mulukeneshetu70@gmail.com>
2024-05-11 11:37:47 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+"""0-subs
+
+Run ./0-main.py programming or ./0-main.py this_is_a_fake_subreddit for testing
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except:
+    """Function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "0-subs.py"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
         return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
