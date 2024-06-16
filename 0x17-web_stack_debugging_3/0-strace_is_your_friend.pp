954903852f8aeea15f4b5809c0208770e28250aa
Muluken <mulukeneshetu70@gmail.com>
2024-06-16 07:55:30 +0000

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# Puppet manifest to correct incorrect `phpp` file extensions to `php` in the WordPress configuration file (`wp-settings.php`)
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`
 
 exec { 'fix-phpp-to-php-in-wp-settings':
-  command   => 'sed -i.bak "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path      => ['/usr/local/bin', '/usr/bin', '/bin', '/usr/sbin', '/sbin'],
-  onlyif    => 'grep -q "phpp" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  onlyif  => 'grep phpp /var/www/html/wp-settings.php',
 }
