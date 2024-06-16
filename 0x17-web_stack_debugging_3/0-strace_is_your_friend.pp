4194d41d3ab810713a59221cb903a88214beabfd
Muluken <mulukeneshetu70@gmail.com>
2024-06-16 07:22:23 +0000

    Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,15 @@
+# Puppet manifest to fix Apache 500 error by restarting Apache service
+
+# Define an exec resource to restart the Apache service
+exec { 'restart-apache':
+  command     => '/usr/sbin/service apache2 restart', 
+  path        => '/usr/bin:/bin',
+  refreshonly => true, # Specify that this exec resource should only run when notified
+}
+
+# Define the Apache service
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  subscribe => Exec['restart-apache'], # Subscribe to the restart-apache exec to trigger restart
+}
