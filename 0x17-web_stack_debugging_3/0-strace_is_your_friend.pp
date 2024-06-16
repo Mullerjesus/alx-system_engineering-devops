d853860845c5c473c9bbef67346f10070008c984
Muluken <mulukeneshetu70@gmail.com>
2024-06-16 07:34:42 +0000

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 
 # Define an exec resource to restart the Apache service
 exec { 'restart-apache':
-  command      => '/usr/sbin/service apache2 restart', 
+  command      => '/usr/sbin/service apache2 restart',
   path         => '/usr/bin:/bin',
   refreshonly  => true, # Specify that this exec resource should only run when notified
 }
